07d1bd28a4868cdc403f4017c240a6360537ef41
Luiz-Vitor-Gomes-Esteves <lvge@aluno.ifnmg.com>
2023-08-28 15:51:01 -0300

    Adicionando imagem2

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <div class="Header-Logo-Enterprise">
             @if ($logo)
                 <!--<img src="{{-- asset('logos/' . $logo->last()) --}}" alt="logo" width="100">-->
-                <img src="{{ secure_asset('logos/' . $logo->last()) }}" alt="logo" width="100">
+                <img src="https://api.freelogodesign.org/assets/thumb/logo/0264ff632cec4cb0b58814b599994637_300.png" alt="logo" width="100">
             @endif
     </div>
     <div class="Header-Logo-User">
