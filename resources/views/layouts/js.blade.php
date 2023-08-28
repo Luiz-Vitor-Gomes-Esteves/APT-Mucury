350bf717d310c4413638f90a84341bc38992a778
Luiz-Vitor-Gomes-Esteves <lvge@aluno.ifnmg.com>
2023-08-27 22:38:32 -0300

    Configurando o build no vite para o uso do css

--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -1,9 +1,13 @@
-@vite(['resources/js/app.js'])
+{{--@vite(['resources/js/app.js'])--}}
+
+
+<script src="{{ asset('build/assets/app-c68ed3a8.js') }}"></script>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
 <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 
 
-    
+
 
